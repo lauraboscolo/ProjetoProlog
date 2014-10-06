5e796c68adb5669f87d38de5a56e5f0d47484fd7
unknown <u12161@LAPA11.academico.cotuca.unicamp.br>
2014-10-06 11:49:38 -0300

    Funcao insere

--- BancoDeConhecimento.pl
+++ BancoDeConhecimento.pl
@@ -17,8 +17,8 @@ materia(7,arquitetura_computadores,6).
 materia(8,microcontroladores,4).
 
 %curriculo(cc,[cm1,cm2,...,cmn]).
-curriculo(1,[1,2,3,4]]).
-curriculo(2,[5,6,7,8]]).
+curriculo(1,[1,2,3,4]).
+curriculo(2,[5,6,7,8]).
 
 % aluno(ra,na).
 aluno(12808,jose).
@@ -29,3 +29,13 @@ aluno(12090,ana).
 % cursa(ra,cc)
 cursa(12909,1).
 cursa(12080,2).
+
+%Exercicio2
+% falta(12909,OQUE).
+% OQUE = [tópicos_em_metodologias_de_programacao]
+
+%Inserir um elemento numa lista.
+insereLista(X,[],[X]).
+insereLista(X,[L|R],[X,L|R]).
+
+falta(RA,CC,LM):-
