b2940e0d6d0bfcd4babc53fd07c0920ab66d9e60
unknown <u12161@LAPA29.academico.cotuca.unicamp.br>
2014-10-07 10:55:17 -0300

    Realizado o exercicio 2 e algumas funções básicas

--- BancoDeConhecimento.pl
+++ BancoDeConhecimento.pl
@@ -10,7 +10,7 @@ curso(6,plasticos).
 materia(1,tecnicas_de_programacao,8).
 materia(2,programacao_orientada_a_objetos,5).
 materia(3,estruturas_de_dados,4).
-materia(4,topicos_em_metodologias_de_programacao).
+materia(4,topicos_em_metodologias_de_programacao,3).
 materia(5,circuitos_eletricos,4).
 materia(6,eletronica_digital,5).
 materia(7,arquitetura_computadores,6).
@@ -30,6 +30,12 @@ aluno(12090,ana).
 cursa(12909,1).
 cursa(12080,2).
 
+%item(cm,sm,an,nt,fq).
+historico(12808,[item(1,1,2012,3.0,0.77),item(1,2,2013,6.5,0.90),item(5,1,2014,8.0,0.80)]).
+historico(12909,[item(1,1,2012,4.0,0.80),item(2,2,2013,8.5,0.80),item(3,1,2014,5.0,0.75)]).
+historico(12080,[item(5,1,2012,6.0,0.70),item(5,2,2013,7.5,0.90),item(6,1,2014,5.0,0.90)]).
+historico(12090,[item(7,1,2012,6.0,0.75),item(8,2,2014,8.0,0.89)]).
+
 %Exercicio2
 % falta(12909,OQUE).
 % OQUE = [tópicos_em_metodologias_de_programacao]
@@ -38,4 +44,33 @@ cursa(12080,2).
 insereLista(X,[],[X]).
 insereLista(X,[L|R],[X,L|R]).
 
-falta(RA,CC,LM):-
+%Existe elemento na lista
+existe(X,[X|_]).
+existe(X,[L|R]):-X\==L,existe(X,R).
+
+%remover
+removerLista(_,[],[]).
+removerLista(X,[X|R],R).
+removerLista(X,[L|R],[L|NL]):-X\==L,removerLista(X,R,NL).
+
+concluiuItem(item(_,_,_,NM,FQ)):-NM@>=5.0,FQ@>=0.75.
+naoConcluiuItem(item(_,_,_,NM,FQ)):-NM@<5.0,FQ@<0.75.
+
+%2
+falta(RA,CC,LM):-historico(RA,H),curriculo(CC,M),retirar(M,H,LM).
+
+%Retirar 2ª da 1ª
+retirar([],[],[]).
+retirar([X|R],[],[X|R]).
+retirar([L|S],[item(COD,_,_,N,F)|R],LISTA):-retirar([L|S],R,NL),existe(COD,[L|S]),concluiuItem(item(COD,_,_,N,F)),removerLista(COD,NL,LISTA).
+retirar([L|S],[item(COD,_,_,_,_)|R],NL):-not(existe(COD,[L|S])),retirar([L|S],R,NL).
+
+%2 FALTA EXTRA
+%2
+extra(RA,CC,LM):-historico(RA,H),curriculo(CC,M),retirarExtra(M,H,LM).
+
+%Retirar 2ª da 1ª
+retirarExtra([],[],[]).
+retirarExtra([X|R],[],[X|R]).
+retirarExtra([L|S],[item(COD,_,_,N,F)|R],LISTA):-retirarExtra([L|S],R,NL),existe(COD,[L|S]),insereLista(COD,NL,LISTA).
+retirarExtra([L|S],[item(COD,_,_,_,_)|R],NL):-not(existe(COD,[L|S])),retirarExtra([L|S],R,NL).
