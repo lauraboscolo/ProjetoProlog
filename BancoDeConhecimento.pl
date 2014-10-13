d440efd3d6efb2cc242441765ac52c0e2249f705
LauraPaixao <laurarpboscolo@gmail.com>
2014-10-13 09:11:10 -0300

    Projeto Pronto!

--- BancoDeConhecimento.pl
+++ BancoDeConhecimento.pl
@@ -16,7 +16,7 @@ materia(6,eletronica_digital,5).
 materia(7,arquitetura_computadores,6).
 materia(8,microcontroladores,4).
 
-% curriculo(cc,[cm1,cm2,...,cmn]).
+%curriculo(cc,[cm1,cm2,...,cmn]).
 curriculo(1,[1,2,3,4]).
 curriculo(2,[7,8]).
 
@@ -31,21 +31,24 @@ cursa(12909,1).
 cursa(12080,2).
 cursa(12090,2).
 
-% historico(ra,itens).
-historico(12808,[item(1,1,2012,3.0,0.77),item(1,2,2013,6.5,0.90),item(5,1,2014,8.0,0.80)]).
-historico(12909,[item(1,1,2012,7.0,0.80),item(2,2,2013,8.5,0.80),item(3,1,2014,5.0,0.75)]).
+%item(cm,sm,an,nt,fq).
+historico(12808,[item(1,1,2012,3.0,0.77),item(1,2,2013,6.5,0.60),item(5,1,2014,8.0,0.80)]).
+historico(12909,[item(1,1,2012,6.0,0.80),item(2,2,2013,8.5,0.80),item(3,1,2014,5.0,0.75)]).
 historico(12080,[item(5,1,2012,6.0,0.70),item(5,2,2013,7.5,0.90),item(6,1,2014,5.0,0.90)]).
-historico(12090,[item(7,1,2012,6.0,0.75),item(8,2,2014,8.0,0.89)]).
+historico(12090,[item(7,1,2012,8.0,0.75),item(8,2,2014,8.0,0.89)]).
 
-concluiuCurso(RA,CC):-cursa(RA,CC),curriculo(CC,CODSCURR),historico(RA,ITENSH),pegarItens(CODSCURR,ITENSCURR),concluiuCurriculo(CC,ITENSCURR,ITENSH).
+
+% EXERCÍCIO 1
+concluiuCurso(RA,CC):-cursa(RA,CC),curriculo(CC,CODSCURR),historico(RA,ITENSH),concluiuCurriculo(RA,ITENSH,CODSCURR).
 
 % (ITENSH,ITENSCURR)
-concluiuCurriculo(HIST,[X|Y]):-materiaConcluida(HIST,X),concluiCurriculo(HIST,Y).
+concluiuCurriculo(_,_,[]).
+concluiuCurriculo(RA,HIST,[X|Y]):-materiaConcluida(RA,HIST,X),concluiuCurriculo(RA,HIST,Y).
 
-% Matéria concluida (ITENS_DO_HISTORICO,CODIGO_MATERIA)
-materiaConcluida([item(CM,S,AN,NT,FQ)|R],CM):-materiaConcluidaAux(Ritem(CM,_,_,NT,FQ),CM).
-materiaConcluida([item(CMA,S,AN,NT,FQ)|R],CM):-CM\==CMA,materiaConcluida(RR,CM).
-materiaConcluidaAux(item(CM,_,_,NT,FQ),CM):-terminouMateria(item(CM,_,_,NT,FQ)).
+% Matéria concluida (RA,ITENS_DO_HISTORICO,CODIGO_MATERIA)
+materiaConcluida(RA,[item(CM,_,_,NT,FQ)|R],CM):-materiaConcluidaAux(RA,item(CM,_,_,NT,FQ),CM).
+materiaConcluida(RA,[item(CMA,_,_,_,_)|R],CM):-CM\==CMA,materiaConcluida(RA,R,CM).
+materiaConcluidaAux(RA,item(CM,_,_,NT,FQ),CM):-terminouMateria(RA,item(CM,_,_,NT,FQ)).
 
 % Terminou Matéria (RA,MATERIA)
 terminouMateria(RA,item(CM,_,_,NT,FQ)):-NT@>=5.0,FQ@>=0.75,temNoHistorico(RA,item(CM,_,_,NT,FQ)).
@@ -64,9 +67,63 @@ pegarItens([],[]).
 pegarItens([P|R],[item(P,_,_,_,_)|Y]):-pegarItens(R,Y).
 
 
-% materiaConcluida(RA,[item(CM,_,_,_,_)|R],CM):-terminouMateria(RA,item(CM,_,_,NT,FQ)).
-% materiaConcluida(RA,[item(CM,_,_,_,_)|R],CMA):-materiaConcluida(RA,R,CMA),CMA\==CM.
-% materiaConcluida(RA,[item(CM,_,_,_,_)|R],CM):-materiaConcluida(RA,R,CM).
+% EXERCÍCIO 2 - MÉTODOS AUXILIARES
+% falta(12909,OQUE).
+% OQUE = [tópicos_em_metodologias_de_programacao]
 
-% concluiuCurriculo(CC,[item(CM,_,_,NT,FQ)]):-curriculo(CC,CM).
-% concluiuCurriculo(CC,[item(CM,_,_,NT,FQ)|R]):-concluiuCurriculo(CC,R),curriculo(CC,CM).
+%Inserir um elemento numa lista.
+insereLista(X,[],[X]).
+insereLista(X,[L|R],[X,L|R]).
+
+%Inserir Unico elemento
+insereUnico(X,[],[X]).
+insereUnico(X,[L|R],[L|R]):-existe(X,[L|R]).
+insereUnico(X,[L|R],[X,L|R]):-not(existe(X,[L|R])).
+
+%Existe elemento na lista
+existe(X,[X|_]).
+existe(X,[L|R]):-X\==L,existe(X,R).
+
+%remover
+removerLista(_,[],[]).
+removerLista(X,[X|R],R).
+removerLista(X,[L|R],[L|NL]):-X\==L,removerLista(X,R,NL).
+
+concluiuItem(item(_,_,_,NM,FQ)):-NM@>=5.0,FQ@>=0.75.
+naoConcluiuItem(item(_,_,_,NM,FQ)):-NM@<5.0,FQ@<0.75.
+
+% Pegar códigos(ITENS,LISTADECODIGOS)
+pegarCodigos([],[]).
+pegarCodigos([item(CM,_,_,_,_)|R],[CM|Q]):-pegarCodigos(R,Q).
+
+%Trasnformar para nome
+transformar([],[]).
+transformar([L|R],NL):-transformar(R,RESP),materia(L,NOME,_),insereLista(NOME,RESP,NL).
+
+% EXERCÍCIO 2
+falta(RA,CC,NM):-historico(RA,H),curriculo(CC,M),retirar(M,H,LM),transformar(LM,NM).
+
+%Retirar 2ª da 1ª Retorna os elementos que faltam para concluir o curso
+retirar([],[],[]).
+retirar([X|R],[],[X|R]).
+retirar([L|S],[item(COD,_,_,_,_)|R],NL):-not(existe(COD,[L|S])),retirar([L|S],R,NL).
+retirar([L|S],[item(COD,_,_,N,F)|R],NL):-not(concluiuItem(item(COD,_,_,N,F))),existe(COD,[L|S]),retirar([L|S],R,NL).
+retirar([L|S],[item(COD,_,_,N,F)|R],LISTA):-existe(COD,[L|S]),concluiuItem(item(COD,_,_,N,F)),retirar([L|S],R,NL),removerLista(COD,NL,LISTA).
+
+
+% EXERCÍCIO 3
+extra(RA,CC,RESP):-historico(RA,H),curriculo(CC,M),pegarCodigos(H,HIST),extraAux(M,HIST,LM),transformar(LM,RESP).
+
+%extraAux(CURRICULO,HISTORICO)
+extraAux([],[],[]).
+extraAux([_|_],[],[]).
+extraAux([L|S],[X|R],LISTA):-extraAux([L|S],R,NL),not(existe(X,[L|S])),insereUnico(X,NL,LISTA).
+extraAux([L|S],[X|R],NL):-existe(X,[L|S]),extraAux([L|S],R,NL).
+
+% EXERCÍCIO 4 - PORCENTAGEM DO CURSO
+
+jafoi(RA,CC,PO):-historico(RA,H),curriculo(CC,M),retirar(M,H,LM),qtos(M,QC),qtos(LM,QFF),PO is (QFF*100/QC).
+
+%contarElementosDaLista
+qtos([],0).
+qtos([_|R],Q):-qtos(R,C),Q is C+1.
